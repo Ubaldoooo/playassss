f7b706d8e07fcf58c5ea80a336f5dbd4fc54afb6
Ubaldoooo <orejuelaolmedoubaldo@gmail.com>
2024-09-16 21:54:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Aquí puedes agregar interactividad en el futuro si es necesario
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("¡Página cargada y lista para explorar!");
+});
